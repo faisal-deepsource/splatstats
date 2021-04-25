3e8950c33f6f1acf53c4707dd8031f32ef036fbe
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2021-04-25 14:58:31 -0400

    style&feat (battle): move textChoices around, finish Weapons textChoices

--- splatstats/models.py
+++ splatstats/models.py
@@ -1,24 +1,11 @@
+from typing import SupportsAbs
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+class Species(models.TextChoices):
+        ink = "inklings", _("Inkling")
+        octo = "octolings", _("Octoling")
 
-class Weapons(models.IntegerChoices):
-    bold = "Sploosh-o-matic"
-    bold_neo = "Neo Sploosh-o-matic"
-    bold_7 = "Sploosh-o-matic 7"
-    wakaba = "Splattershot Jr."
-    momiji = "Custom Splattershot Jr."
-    ochiba = "Kensa Splattershot Jr."
-    sharp = "Splash-o-matic"
-    sharp_neo = "Neo Splash-o-matic"
-    promodeler_mg = "Aerospray MG"
-    promodeler_rg = "Aerospray RG"
-    promodeler_pg = "Aerospray PG"
-    sshooter = "Splattershot"
-    sshooter_collabo = "Tentatek Splattershot"
-    sshooter_becchu = "Kensa Splattershot"
-    heroshooter_replica = "Hero Shot Replica"
-    octoshooter_replica = "Octo Shot Replica"
-    five2gal = ".52 Gal"
-    five2gal_deco = ".52 Gal Deco"
-    five2gal_becchu = "Kensa .52 Gal"
+class Gender(models.TextChoices):
+    girl = "girl", _("Female")
+    boy = "boy", _("Male")
