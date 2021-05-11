352dff5b4184518bcf64f5c1ee3973f34b0cd9a9
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2021-05-11 18:24:40 -0400

    switch to autoinit for materialize components

--- static/core/js/base.js
+++ static/core/js/base.js
@@ -1,4 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, {closeOnClick: true});
-  });
+M.AutoInit();
