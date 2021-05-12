cca1b9e187d0c1fe6529477eac8b3fa989d3633c
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2021-05-12 12:48:00 -0400

    update sendgrid, remove sendgrid-django

--- splatstats/apps/site_auth/views.py
+++ splatstats/apps/site_auth/views.py
@@ -7,7 +7,6 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .forms import SignUpForm
 from .tokens import account_activation_token
-from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 
 
